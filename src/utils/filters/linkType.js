1997aa9bd129755f1cae3238ebed8666e8d498cd
Ed Johnson-Williams <edjw@users.noreply.github.com>
2020-03-22 15:04:37 +0000

    fix email links in voucher/delivery links

--- src/utils/filters/linkType.js
+++ src/utils/filters/linkType.js
@@ -1,14 +1,16 @@
 module.exports = function (link) {
-
-    // Super hacky way to detect if it's a phone number or not
+    var linkType = "";
+    // Super hacky way to detect if it's a phone number or not. But do any other kinds of links start with a + or a 0?
     if (link.startsWith("+") || link.startsWith("0")) {
-        return "phone";
+        linkType = "phone";
+    }
+
+    // Super hacky way to detect if it's an email or not. But do any other kinds of links include an @ symbol?
+    else if (link.includes("@")) {
+        linkType = "email";
     }
     else {
-        var emailAddressRegex = new RegExp(`/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/`);
-        var lowerCaseLink = String(link).toLowerCase();
-        if (emailAddressRegex.test(lowerCaseLink)) {
-            return "email";
-        }
+        linkType = "webpage";
     }
+    return linkType;
 }
