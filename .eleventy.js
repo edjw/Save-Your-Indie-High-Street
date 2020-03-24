b2efefc2fc4c0915177dc0039f2fe6b0b0b66146
Ed Johnson-Williams <edjw@users.noreply.github.com>
2020-03-24 16:33:45 +0000

    different favicons in production and development

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,7 @@ module.exports = function (eleventyConfig) {
   let markdownLib = markdownIt(options);
   eleventyConfig.setLibrary("md", markdownLib);
 
-  // A useful way to reference the context we are runing eleventy in
+  // A useful way to reference the context we are running eleventy in
   let env = process.env.ELEVENTY_ENV;
 
   // Layout aliases can make templates more portable
@@ -20,7 +20,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("squash", require("./src/utils/filters/squash.js"));
 
   eleventyConfig.addFilter("linkType", require("./src/utils/filters/linkType.js"));
-  
+
   eleventyConfig.addFilter("slugIt", require("./src/utils/filters/slugIt.js"));
 
   const CleanCSS = require("clean-css");
@@ -46,8 +46,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("_redirects");
   eleventyConfig.addPassthroughCopy("_headers");
 
-  eleventyConfig.addPassthroughCopy({ "./src/site/assets/images/favicons": "/" });
-
+  if (env === "prod") {
+    eleventyConfig.addPassthroughCopy({ "./src/site/assets/images/favicons_prod": "/" });
+  } else if (env === "dev") {
+    eleventyConfig.addPassthroughCopy({ "./src/site/assets/images/favicons_dev": "/" });
+  }
 
   return {
     dir: {
