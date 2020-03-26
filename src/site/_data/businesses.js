88b930f755ffaf1ecae097e4100f529c57593bae
Ed Johnson-Williams <edjw@users.noreply.github.com>
2020-03-26 22:57:01 +0000

    make links without http at the start work

--- src/site/_data/businesses.js
+++ src/site/_data/businesses.js
@@ -7,6 +7,23 @@ Airtable.configure({
 });
 const base = Airtable.base(process.env.AIRTABLE_BASE_ID);
 
+function linkType (link) {
+    var linkType = "";
+    // Super hacky way to detect if it's a phone number or not. But do any other kinds of links start with a + or a 0?
+    if (link.startsWith("+") || link.startsWith("0")) {
+        linkType = "phone";
+    }
+
+    // Super hacky way to detect if it's an email or not. But do any other kinds of links include an @ symbol?
+    else if (link.includes("@")) {
+        linkType = "email";
+    }
+    else {
+        linkType = "webpage";
+    }
+    return linkType;
+}
+
 module.exports = async () => {
     let allRecords = [];
     return new Promise((resolve, reject) => {
@@ -27,6 +44,18 @@ module.exports = async () => {
 
                     if (record.fields.town_city.toLowerCase() === "newcastle upon tyne") {
                         record.fields.town_city = "Newcastle";
+                    }
+
+                    if (record.fields.website && record.fields.website.startsWith("http") == false) {
+                        record.fields.website = `http://${record.fields.website}`;
+                    }
+
+                    if (record.fields.voucher_link && linkType(record.fields.voucher_link) == "webpage" && record.fields.voucher_link.startsWith("http") == false) {
+                        record.fields.voucher_link = `http://${record.fields.voucher_link}`;
+                    }
+
+                    if (record.fields.delivery_link && linkType(record.fields.delivery_link) == "webpage" && record.fields.delivery_link.startsWith("http") == false) {
+                        record.fields.delivery_link = `http://${record.fields.delivery_link}`;
                     }
 
                     allRecords.push(record.fields);
